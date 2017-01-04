4b6c4f5b03f32b312e6cc4d0fadda7b832645404
Sanjee <sanjee.yogeswaran@gmail.com>
2017-01-04 01:52:21 -0600

    revamped history link for posts

--- routes/index.js
+++ routes/index.js
@@ -30,9 +30,14 @@ router.get('/wiimodchip',function(req,res){
 router.get('/:id', function(req, res, next) {
   visitor.pageview("/").send()
     db.serialize(function(){
-        db.all("SELECT post,date,time FROM posts WHERE rowid <= "+req.params.id+" ORDER BY rowid DESC LIMIT 5",function(err, row){
-        res.render('index', { title: 's4njee',posts:row, current:req.params.id});
+        db.each("SELECT rowid AS id FROM posts ORDER BY rowid DESC LIMIT 1",function(err, row){
+            db.all("SELECT post,date,time FROM posts WHERE rowid <= "+(row.id-req.params.id)+" ORDER BY rowid DESC LIMIT 5",function(err, row){
+                console.log(row.length)
+                if(row.length<5){ 
+                res.render('index', { title: 's4njee',posts:row, current:req.params.id, lastpage:true});
+                } 
+               });
+            });
         });
-    });
 });
 module.exports = router;
